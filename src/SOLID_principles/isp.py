5adf6bb0911ab7394a78f1ec60c4e11b502f2074
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2023-05-13 14:14:32 +0530

    adding code for Interface Segregation Principle (ISP)

---
+++ src/SOLID_principles/isp.py
@@ -0,0 +1,8 @@
+"""
+class containing the sample implementation of
+Interface Segregation Principle (ISP)
+
+Basically, the principle is similar to the single responsibility principle
+in the sense that classes should only implement methods that they actually use.
+"""
+
