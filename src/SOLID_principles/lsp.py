e17cbc4c222fc7c7bdcda7dd65cfc49ddb6d7f8f
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2023-05-13 13:12:37 +0530

    adding code for liskov substitution principle

---
+++ src/SOLID_principles/lsp.py
@@ -0,0 +1,6 @@
+"""
+class containing the implementation of liskov Substitution principe.
+
+It should be possible to replace an instance of a superclass with
+an instance of a subclass without causing breaking changes.
+"""
