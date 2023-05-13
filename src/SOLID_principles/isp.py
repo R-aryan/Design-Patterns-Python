b3e10363105beb7e38dc8da15a7433a552932fbc
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2023-05-13 14:15:13 +0530

    adding code for Interface Segregation Principle (ISP)

--- src/SOLID_principles/isp.py
+++ src/SOLID_principles/isp.py
@@ -6,3 +6,27 @@ Basically, the principle is similar to the single responsibility principle
 in the sense that classes should only implement methods that they actually use.
 """
 
+from abc import abstractmethod
+
+
+class Machine:
+    def print(self, document):
+        raise NotImplementedError()
+
+    def fax(self, document):
+        raise NotImplementedError()
+
+    def scan(self, document):
+        raise NotImplementedError()
+
+
+# ok if you need a multifunction device
+class MultiFunctionPrinter(Machine):
+    def print(self, document):
+        pass
+
+    def fax(self, document):
+        pass
+
+    def scan(self, document):
+        pass
