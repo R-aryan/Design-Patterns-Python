9ff9662ced3a1851bf13464732c2191c29dd2792
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2023-05-13 14:33:07 +0530

    adding code for  Dependency Inversion Principle(DIP)

---
+++ src/SOLID_principles/dip.py
@@ -0,0 +1,7 @@
+"""
+class containing the implementation of Dependency Inversion Principle(DIP)
+Robert C. Martin’s definition of the Dependency Inversion Principle consists of two parts:
+
+1. High-level modules should not depend on low-level modules. Both should depend on abstractions.
+2. Abstractions should not depend on details. Details should depend on abstractions.
+"""
