65582bfbf24e3ac65191408224cbec5671870f9b
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2023-05-14 14:55:32 +0530

    added code for builder pattern

--- src/builder_pattern/builder.py
+++ src/builder_pattern/builder.py
@@ -32,3 +32,30 @@ class HtmlElement:
     @staticmethod
     def create(name):
         return HtmlBuilder(name)
+
+
+class HtmlBuilder:
+    __root = HtmlElement()
+
+    def __init__(self, root_name):
+        self.root_name = root_name
+        self.__root.name = root_name
+
+    # not fluent
+    def add_child(self, child_name, child_text):
+        self.__root.elements.append(
+            HtmlElement(child_name, child_text)
+        )
+
+    # fluent
+    def add_child_fluent(self, child_name, child_text):
+        self.__root.elements.append(
+            HtmlElement(child_name, child_text)
+        )
+        return self
+
+    def clear(self):
+        self.__root = HtmlElement(name=self.root_name)
+
+    def __str__(self):
+        return str(self.__root)
