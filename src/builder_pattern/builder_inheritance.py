5d66a8e1eab43fd0fc848dd00efd21a8ad6c8ce0
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2023-05-15 13:29:26 +0530

    added code for builder inheritance

---
+++ src/builder_pattern/builder_inheritance.py
@@ -0,0 +1,20 @@
+class Person:
+    def __init__(self):
+        self.name = None
+        self.position = None
+        self.date_of_birth = None
+
+    def __str__(self):
+        return f'{self.name} born on {self.date_of_birth} works as a {self.position}'
+
+    @staticmethod
+    def new():
+        return PersonBuilder()
+
+
+class PersonBuilder:
+    def __init__(self):
+        self.person = Person()
+
+    def build(self):
+        return self.person
