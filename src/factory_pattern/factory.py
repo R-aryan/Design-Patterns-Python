c82eeda3ac87af0f3ffc7a3ccd56aaf1cd335a5c
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2023-05-28 14:53:49 +0530

    added code for factory pattern

--- src/factory_pattern/factory.py
+++ src/factory_pattern/factory.py
@@ -1,3 +1,35 @@
 """
 class containing the implementation of sample factory design pattern example
 """
+
+from enum import Enum
+from math import *
+
+
+class CoordinateSystem(Enum):
+    CARTESIAN = 1
+    POLAR = 2
+
+
+class Point:
+    # def __init__(self, x, y):
+    #     self.x = x
+    #     self.y = y
+
+    def __str__(self):
+        return f'x: {self.x}, y: {self.y}'
+
+    # redeclaration won't work
+    # def __init__(self, rho, theta):
+
+    def __init__(self, a, b, system=CoordinateSystem.CARTESIAN):
+        if system == CoordinateSystem.CARTESIAN:
+            self.x = a
+            self.y = b
+        elif system == CoordinateSystem.POLAR:
+            self.x = a * sin(b)
+            self.y = a * cos(b)
+
+        # steps to add a new system
+        # 1. augment CoordinateSystem
+        # 2. change init method
